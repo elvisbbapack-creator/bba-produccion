62f72f3456ee280f532e08c5ac793c33a17cb12f
Elvis Saavedra <elvissaavedra@MacBook-Pro-de-Elvis.local>
2026-05-11 00:48:32 -0400

    dashboard tv

--- src/App.js
+++ src/App.js
@@ -1621,22 +1621,6 @@ if (pantalla === "otDetalle") {
                 {" "}
                 {eficiencia}%
               </div>
-
-              <input
-                type="number"
-                placeholder="Cantidad OK"
-                onChange={(e) => {
-                  p.cantidadFinal = e.target.value;
-                }}
-                style={{
-                  width: "100%",
-                  padding: 10,
-                  marginTop: 10,
-                  borderRadius: 6,
-                  border: "1px solid #ccc",
-                  boxSizing: "border-box"
-                }}
-              />
   
             </div>
 
